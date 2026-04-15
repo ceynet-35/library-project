20d7ed1260c3ef71ef2a323baafba7f681d1eac6
matyatya31 <mat09.06nik@gmail.com>
2026-04-15 13:03:36 +0300

    Update LibraryCode.py

--- LibraryCode.py
+++ LibraryCode.py
@@ -11,7 +11,7 @@ class Item:
         self.title=title     # название объекта
         self.popularity=0    # популярность объекта (будет увиличиваться когда предмед будут брать пример 7.5./10)
         self.amount=amount         # сколько всего экземпляров в библеотеке
-        self.available=amount      # сколько сейчас доступно для выдачи 1
+        self.available=amount      # сколько сейчас доступно для выдачи 
 
 class Book(Item):
     def __init__(self, item_id, title, day_arent=14):
